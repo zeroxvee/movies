c9e9a8c716a2ad6d65276865c46652a1ec05f737
Manav Misra <manavm@visionwebsoft.com>
2020-08-07 17:56:50 -0500

    :fire: Remove unused code

--- src/components/MovieSearch/Form/index.js
+++ src/components/MovieSearch/Form/index.js
@@ -3,21 +3,13 @@ import React from "react"
 
 import "./Form.css"
 
-export const Form = ({ handler }) => {
+export const Form = ({ handler }) => (
+  <form onSubmit={handler}>
+    <input></input>
+    <button type="Submit">Search</button>
+  </form>
+)
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    handler(event)
-  }
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input></input>
-      <button type="Submit">Search</button>
-    </form>
-  )
-}
-
-Form.propType = {
+Form.propTypes = {
   handler: PropType.func,
 }
--- src/components/MovieSearch/index.js
+++ src/components/MovieSearch/index.js
@@ -9,31 +9,11 @@ import api from "api"
 
 export const MovieSearch = () => {
   const [movies, SetMovies] = useState([])
-  const [similarMovies, setSimilarMovies] = useState([])
 
   const searchHandler = async (event) => {
     // we only need the results property
     const { results } = await api.index(event.target.elements[0].value)
     SetMovies(results)
-  }
-
-  // const fetchSimilar = async (event) => {
-  //   // we only need the results property
-  //   const {results} = await api.similar(event)
-  //   SetMovies(results)
-  // }
-
-  const showSimilarSection = async (id) => {
-    const { results } = await api.similar(id)
-    setSimilarMovies(results)
-    return (
-      results.map((movie, i) => (
-        <div key={i}>
-          <p>{movie.title}</p>
-        </div>
-      )
-      )
-    )
   }
 
   return (
@@ -45,5 +25,3 @@ export const MovieSearch = () => {
     </main>
   )
 }
-
-//showSimilar={showSimilarSection}
